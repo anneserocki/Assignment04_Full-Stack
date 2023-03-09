3d9a94c2360e2cc7775735d3fc28caa772dc4617
Your Name <anneserocki@gmail.comom>
2023-03-09 12:51:29 -0800

    finished final version

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,8 +1,46 @@
 // IMPORT THE MODULE
+// import * as calculator from './modules/calculator.js';
+import {add} from './modules/calculator.js';
+import {sub} from './modules/calculator.js';
+import {mul} from './modules/calculator.js';
+import {div} from './modules/calculator.js';
 
 // COLLECT FIRST NUMBER FROM USER
+let num1
+do {
+    num1 = Number(prompt('Enter the first number: '));
+  } while (isNaN(num1));
 // COLLECT SECOND NUMBER FROM USER
+let num2
+do {
+    num2 = Number(prompt('Enter the second number: '));
+  } while (isNaN(num2));
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
+let operator
+do {
+    operator = prompt('Enter the operator (+, -, *, /): ');
+  } while (!['+', '-', '*', '/'].includes(operator));
 
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
+let calcResult = 0
+switch(operator) {
+  case '+':
+    calcResult = add(num1, num2);
+    break;
+  case '-':
+    calcResult = sub(num1, num2);
+    break;
+  case '*':
+    calcResult = mul(num1, num2);
+    break;
+  case '/':
+    calcResult = div(num1, num2);
+    break;
+}
+
+if (isNaN(calcResult)) {
+  alert('Invalid operator!');
+} else {
+  alert(`The result is: ${calcResult}`);
+}
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,13 +1,44 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
+const calculate = function (num1, num2, operator) {
+  switch(operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      return num1 / num2;
+    default:
+      return NaN;
+  };
+};
 
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 //////////////////////////////////
 // ADD FUNCTION
-
+const add = function (num1, num2) {
+  calculate(num1, num2, '+')
+}
+    
 // SUBTRACT FUNCTION
+const sub = function (num1, num2) {
+  calculate(num1, num2, '-')
+}
 
 // MULTIPLY FUNCTION
+const mul = function (num1, num2) {
+  calculate(num1, num2, '*')
+}
 
 // DIVIDE FUNCTION
+const div = function (num1, num2) {
+  calculate(num1, num2, '/')
+}
 
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+
+export {add};
+export {sub};
+export {mul};
+export {div};
